68cdc09da136ded28e3fc999b95902566f3dcea4
Yash-Sharma2002 <93969847+Yash-Sharma2002@users.noreply.github.com>
2024-02-09 20:42:18 +0000

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from "react";
-import Main from "./pages/Main";
-import TermsConditions from "./pages/TermsConditions";
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
 import Footer from "./components/Footer";
+
+const Main = React.lazy(() => import('./pages/Main'))
+const About = React.lazy(() => import('./pages/About'))
+const TermsConditions = React.lazy(() => import('./pages/TermsConditions'))
+const Policies = React.lazy(() => import('./pages/Policies'))
 
 function App() {
   return (
     <>
-      <Main />
-  <Footer />
+      <BrowserRouter >
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/terms-and-conditions" element={<TermsConditions />} />
+          <Route path="/policies" element={<Policies />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </>
   );
 }
